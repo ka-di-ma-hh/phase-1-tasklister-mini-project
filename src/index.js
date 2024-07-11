fc9cb179ee079d81688ff1bac54746484f875044
junne.wanja@student.moringaschool.com <junne.wanja@student.moringaschool.com>
2024-07-11 20:29:12 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+let form =document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("createTaskForm")
+    createTaskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target.newTaskDescription.value);
+createTaskForm.reset()
+  })
 });
+function handleToDo(todo) {
+  let p = document.createElement('p')
+  p.textContent = `${todo} `
+  tasks.appendChild(p)
+  let btn = document.createElement('button')
+  btn.addEventListener('click',deleteTask)
+  btn.textContent = 'x'
+  p.appendChild(btn)
+  
+
+}
+function deleteTask(e) {
+  e.target.parentNode.remove()
+ }
+ 
+  
+
